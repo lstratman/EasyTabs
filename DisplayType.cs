0b25f9a4a050a6445fc3262490dceaf50f83e0cf
lstratman <lstratman@gmail.com>
2012-04-23 15:31:26 -0400

    Added the missing DisplayType enumeration.

---
+++ DisplayType.cs
@@ -0,0 +1,21 @@
+﻿namespace Stratman.Windows.Forms.TitleBarTabs
+{
+	/// <summary>
+	/// The type of theme being used to render the desktop.
+	/// </summary>
+	public enum DisplayType
+	{
+		/// <summary>
+		/// Windows 2000-esque theme.
+		/// </summary>
+		Classic,
+		/// <summary>
+		/// Contemporary theme, but without Aero enabled.
+		/// </summary>
+		Basic,
+		/// <summary>
+		/// Full compositing enabled in the theme.
+		/// </summary>
+		Aero
+	}
+}
