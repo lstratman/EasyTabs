bc09a61f4bb9d5848f51d8d6ea1254ec6c23e22e
Luke Stratman <lstratman@gmail.com>
2019-05-27 12:52:04 +0700

    Updated Chrome tab renderer resources

--- ChromeTabRenderer.cs
+++ ChromeTabRenderer.cs
@@ -5,11 +5,6 @@ namespace EasyTabs
 	/// <summary>Renderer that produces tabs that mimic the appearance of the Chrome browser.</summary>
 	public class ChromeTabRenderer : BaseTabRenderer
 	{
-        /// <summary>
-        /// A Chrome-specific right-side tab image that allows the separation between inactive tabs to be more clearly defined.
-        /// </summary>
-	    protected Image _inactiveRightSideShadowImage = Resources.ChromeInactiveRightShadow;
-
 		/// <summary>Constructor that initializes the various resources that we use in rendering.</summary>
 		/// <param name="parentWindow">Parent window that this renderer belongs to.</param>
 		public ChromeTabRenderer(TitleBarTabs parentWindow)
@@ -29,38 +24,24 @@ namespace EasyTabs
 			_addButtonHoverImage = new Bitmap(Resources.ChromeAddHover);
 
 			// Set the various positioning properties
-			CloseButtonMarginTop = 6;
+			CloseButtonMarginTop = 9;
 			CloseButtonMarginLeft = 2;
-			AddButtonMarginTop = 7;
+            CloseButtonMarginRight = 4;
+			AddButtonMarginTop = 3;
 			AddButtonMarginLeft = -1;
-			CaptionMarginTop = 6;
-			IconMarginTop = 7;
+			CaptionMarginTop = 9;
+            IconMarginLeft = 9;
+			IconMarginTop = 9;
 			IconMarginRight = 5;
 			AddButtonMarginRight = 5;
 		}
-
-        /// <summary>
-        /// Gets the image to use for the right side of the tab.  For Chrome, we pick a specific image for inactive tabs that aren't at
-        /// the end of the list to allow for the separation between inactive tabs to be more clearly defined.
-        /// </summary>
-        /// <param name="tab">Tab that we are retrieving the image for.</param>
-        /// <returns>Right-side image for <paramref name="tab"/>.</returns>
-	    protected override Image GetTabRightImage(TitleBarTab tab)
-	    {
-	        ListWithEvents<TitleBarTab> allTabs = tab.Parent.Tabs;
-
-            if (tab.Active || allTabs.IndexOf(tab) == allTabs.Count - 1)
-	            return base.GetTabRightImage(tab);
-
-	        return _inactiveRightSideShadowImage;
-	    }
 
 	    /// <summary>Since Chrome tabs overlap, we set this property to the amount that they overlap by.</summary>
 		public override int OverlapWidth
 		{
 			get
 			{
-				return 15;
+				return 14;
 			}
 		}
 	}
--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace EasyTabs {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -146,16 +146,6 @@ namespace EasyTabs {
         internal static System.Drawing.Bitmap ChromeInactiveRight {
             get {
                 object obj = ResourceManager.GetObject("ChromeInactiveRight", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ChromeInactiveRightShadow {
-            get {
-                object obj = ResourceManager.GetObject("ChromeInactiveRightShadow", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
